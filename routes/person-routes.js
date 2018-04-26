005ab0a6e04e36a3e82d8351539f8a7325a63e4d
jarnovk <34368144+jsklaarb@users.noreply.github.com>
2018-04-26 14:37:36 +0200

    asdf

--- routes/person-routes.js
+++ routes/person-routes.js
@@ -1,1 +1,31 @@
-//
+
+
+app.get('/api/person', (req, res, next) => {
+	res.status(200).json(personlist).end();
+});
+
+app.get('/api/person/:id', (req, res, next) => {
+	const id = req.params.id;
+
+  // als id geldig is in de personlist: stuur person terug
+	if(id >= 0 && id < personlist.length){
+		res.status(200).json(personlist[id]).end();
+	} else { // als id niet geldig is: error
+		const error = {
+			error: 'ID does not exist (index out of bounds)'
+		}
+		next(error);
+	}
+});
+
+app.post('/api/person', (req, res, next) => {
+	console.log(req.body);
+
+	const firstname = req.body.firstname;
+	const lastname = req.body.lastname;
+	const person = new Person(firstname, lastname);
+  
+	personlist.push(person);
+
+	res.status(200).json(person).end();
+});
--- server.js
+++ server.js
@@ -20,35 +20,7 @@ app.use('*', (req, res, next) => {
   next();
 });
 
-app.get('/api/person', (req, res, next) => {
-	res.status(200).json(personlist).end();
-});
 
-app.get('/api/person/:id', (req, res, next) => {
-	const id = req.params.id;
-
-  // als id geldig is in de personlist: stuur person terug
-	if(id >= 0 && id < personlist.length){
-		res.status(200).json(personlist[id]).end();
-	} else { // als id niet geldig is: error
-		const error = {
-			error: 'ID does not exist (index out of bounds)'
-		}
-		next(error);
-	}
-});
-
-app.post('/api/person', (req, res, next) => {
-	console.log(req.body);
-
-	const firstname = req.body.firstname;
-	const lastname = req.body.lastname;
-	const person = new Person(firstname, lastname);
-  
-	personlist.push(person);
-
-	res.status(200).json(person).end();
-});
 
   // Wanneer de gevraagde endpoint niet gevonden is komen we hier.
 app.use('*', (req, res, next) => {
